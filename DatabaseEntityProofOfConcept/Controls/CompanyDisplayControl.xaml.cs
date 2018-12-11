0fc8c93b331ad99e99dbb9523cb172d759991ffc
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2018-12-11 17:31:05 +0000

    Added displaying user control templates

---
+++ DatabaseEntityProofOfConcept/Controls/CompanyDisplayControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DatabaseEntityProofOfConcept.Controls
+{
+    public partial class CompanyDisplayControl : UserControl
+    {
+        public IEnumerable Companies
+        {
+            get { return (IEnumerable)GetValue(CompaniesProperty); }
+            set { SetValue(CompaniesProperty, value); }
+        }
+        public static readonly DependencyProperty CompaniesProperty =
+            DependencyProperty.Register("Companies", typeof(IEnumerable), typeof(CompanyDisplayControl), new PropertyMetadata(null));
+
+        public CompanyDisplayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
